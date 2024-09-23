d47a392f7cf057ec551738a730eeae6b6665c3fc
LauraChatard <155462678+LauraChatard@users.noreply.github.com>
2024-09-23 12:16:44 +0200

    Update app.py

--- Implementez_un_modele_de_scoring/app.py
+++ Implementez_un_modele_de_scoring/app.py
@@ -106,5 +106,5 @@ def predict():
         print("Exception occurred:", str(e))
         return jsonify({"error": str(e)}), 400
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5003)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
