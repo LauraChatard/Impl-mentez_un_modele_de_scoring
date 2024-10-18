b5722a768cb81d2708022a5ab0eeecb8d0589518
LauraChatard <155462678+LauraChatard@users.noreply.github.com>
2024-10-18 12:03:26 +0200

    Update app.py

--- Implementez_un_modele_de_scoring/app.py
+++ Implementez_un_modele_de_scoring/app.py
@@ -56,7 +56,7 @@ def load_json_from_s3(file_name):
         raise  # Re-raise the exception after logging
 
 # Load the client data 
-client_data = load_json_from_s3('data_dashboard.json')  # Load JSON data
+client_data = load_json_from_s3('data_dashboard.json')  # Chargez le bon fichier depuis S3
 
 def load_client_info(s3_uri, client_id):
     try:
