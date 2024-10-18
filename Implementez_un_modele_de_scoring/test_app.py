5dbc472f6c0edf8dd1cd04fd27d7a0370d082aee
LauraChatard <155462678+LauraChatard@users.noreply.github.com>
2024-10-18 14:02:41 +0200

    Update test_app.py

--- Implementez_un_modele_de_scoring/test_app.py
+++ Implementez_un_modele_de_scoring/test_app.py
@@ -8,11 +8,11 @@ class TestFlaskApp(unittest.TestCase):
         cls.app = app.test_client()  # Create a test client
         cls.app.testing = True  # Enable testing mode
 
-    #def test_predict_valid_client(self):
+    def test_predict_valid_client(self):
         # Test with a valid client ID
-        #response = self.app.post('/predict', json={'SK_ID_CURR': 102552})  
-        #self.assertEqual(response.status_code, 200)
-        #self.assertIn('decision', response.get_json())  # Check that the 'decision' key is in the response
+        response = self.app.post('/predict', json={'SK_ID_CURR': 264862})  
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('decision', response.get_json())  # Check that the 'decision' key is in the response
 
     #def test_predict_invalid_client(self):
         # Test with an invalid client ID
