28420ab44bb89adb9e3bf7affa9b5c1794bdc9ab
LauraChatard <155462678+LauraChatard@users.noreply.github.com>
2024-10-22 13:59:22 +0200

    Update test_app.py

--- Implementez_un_modele_de_scoring/test_app.py
+++ Implementez_un_modele_de_scoring/test_app.py
@@ -14,29 +14,17 @@ class TestFlaskApp(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('decision', response.get_json())  # Check that the 'decision' key is in the response
 
-    def test_predict_valid_client_2(self):
-        test_data = {
-            'SK_ID_CURR': 264862  # Remplacez par un ID client valide
-        }
-        
-        # Vérifier que les données de test sont au bon format
-        self.assertIn('SK_ID_CURR', test_data)
-        self.assertIsInstance(test_data['SK_ID_CURR'], int)
-    
-        response = self.client.post('/predict', json=test_data)
-        self.assertEqual(response.status_code, 200)
-
-    #def test_predict_invalid_client(self):
+    def test_predict_invalid_client(self):
         # Test with an invalid client ID
-        #response = self.app.post('/predict', json={'SK_ID_CURR': 99999})  # ID that does not exist
-        #self.assertEqual(response.status_code, 404)
-        #self.assertIn('error', response.get_json())  # Check that the 'error' key is in the response
+        response = self.app.post('/predict', json={'SK_ID_CURR': 99999})  # ID that does not exist
+        self.assertEqual(response.status_code, 404)
+        self.assertIn('error', response.get_json())  # Check that the 'error' key is in the response
 
-    #def test_predict_missing_client_id(self):
+    def test_predict_missing_client_id(self):
         # Test without the client ID
-        #response = self.app.post('/predict', json={})
-        #self.assertEqual(response.status_code, 400)
-        #self.assertIn('error', response.get_json())  # Check that the 'error' key is in the response
+        response = self.app.post('/predict', json={})
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('error', response.get_json())  # Check that the 'error' key is in the response
 
 if __name__ == '__main__':
     unittest.main()  # Run the tests
