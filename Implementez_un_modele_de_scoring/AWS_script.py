00ec53947ba56aed08a3d29d8bdd9e7efd8468ac
LauraChatard <lau.chatard@gmail.com>
2024-10-31 10:11:49 +0100

    shap

--- Implementez_un_modele_de_scoring/AWS_script.py
+++ Implementez_un_modele_de_scoring/AWS_script.py
@@ -257,179 +257,179 @@ if st.button("Get Prediction"):
         else:
             st.error("Erreur de format dans la réponse de l'API.")
 
-    # Client Info collapsible section in sidebar
-    if st.session_state.prediction_data and "error" not in st.session_state.prediction_data:
-        with st.sidebar.expander("Client Info"):
-            if client_id:
-                if st.session_state.client_info is None:
-                    response_data = get_client_info(client_id)  # Appel de la fonction
-                    st.session_state.client_info = response_data  # Stocker la réponse complète
-                    
-                if "error" not in st.session_state.client_info:
-                    client_info = st.session_state.client_info.get("client_info")  # Accéder à client_info
-                    all_clients = st.session_state.client_info.get("all_clients")  # Accéder à all_clients
-
-                    # Créer un dictionnaire avec les informations du client
-                    client_info_data = {
-                        "Attribute": [
-                            "Age",
-                            "Gender",
-                            "Income Type",
-                            "Loan Type",
-                            "Children",
-                            "Income"
-                        ],
-                        "Value": [
-                            f"{client_info['age']} ans",
-                            client_info['CODE_GENDER'],
-                            client_info['NAME_INCOME_TYPE'],
-                            client_info['NAME_CONTRACT_TYPE'],
-                            client_info['CNT_CHILDREN'],
-                            f"{client_info['AMT_INCOME_TOTAL']:,.0f} €".replace(',', ' ')
-                        ]
-                    }
-
-                    # Convertir en DataFrame
-                    client_info_df = pd.DataFrame(client_info_data)
-
-                    # Transposer le DataFrame
-                    client_info_df = client_info_df.set_index("Attribute")
-
-                    # Afficher le tableau dans la barre latérale
-                    st.sidebar.table(client_info_df)
-
-                    # Récupérer les données pour la visualisation
-                    client_income = client_info['AMT_INCOME_TOTAL']
-                    client_credit = client_info['AMT_CREDIT']
-                    client_age = client_info['age']
-                    client_children = client_info['CNT_CHILDREN']
-
-                    # Récupérer les données de tous les clients pour la comparaison
-                    all_clients = st.session_state.client_info.get("all_clients", [])
-                    all_clients_df = pd.DataFrame(all_clients)
-                    labels = ['Accepted', 'Rejected', 'Client']    
-
-                    # Enregistrer les données dans session_state
-                    st.session_state.income_comparison_data = (labels, client_income, client_credit, client_age, client_children)
-
-                else:
-                    st.sidebar.error(st.session_state.client_info["error"])
-                st.write("Please enter a Client ID and click 'Get Prediction'.")
+# Client Info collapsible section in sidebar
+if st.session_state.prediction_data and "error" not in st.session_state.prediction_data:
+    with st.sidebar.expander("Client Info"):
+        if client_id:
+            if st.session_state.client_info is None:
+                response_data = get_client_info(client_id)  # Appel de la fonction
+                st.session_state.client_info = response_data  # Stocker la réponse complète
                 
-        # Paramètres de couleur pour le fond noir et le texte blanc
-        plt.style.use('dark_background')
+            if "error" not in st.session_state.client_info:
+                client_info = st.session_state.client_info.get("client_info")  # Accéder à client_info
+                all_clients = st.session_state.client_info.get("all_clients")  # Accéder à all_clients
 
-        # Bouton pour afficher la comparaison des caractéristiques du client
-        if st.sidebar.button("Compare Client") and "income_comparison_data" in st.session_state:
-            # Récupérer les données de comparaison
-            labels, client_income, client_credit, client_age, client_children = st.session_state.income_comparison_data
+                # Créer un dictionnaire avec les informations du client
+                client_info_data = {
+                    "Attribute": [
+                        "Age",
+                        "Gender",
+                        "Income Type",
+                        "Loan Type",
+                        "Children",
+                        "Income"
+                    ],
+                    "Value": [
+                        f"{client_info['age']} ans",
+                        client_info['CODE_GENDER'],
+                        client_info['NAME_INCOME_TYPE'],
+                        client_info['NAME_CONTRACT_TYPE'],
+                        client_info['CNT_CHILDREN'],
+                        f"{client_info['AMT_INCOME_TOTAL']:,.0f} €".replace(',', ' ')
+                    ]
+                }
 
-            # Extraire uniquement les couleurs 'accepted' et 'rejected'
-            colors = {'accepted': ACCESSIBLE_COLORS['accepted'], 'rejected': ACCESSIBLE_COLORS['rejected']}
-            text_color = ACCESSIBLE_COLORS['text']
+                # Convertir en DataFrame
+                client_info_df = pd.DataFrame(client_info_data)
 
-            # Graphiques
-            if not all_clients_df.empty:
-                # 1. Graphique de distribution entre Income et Credit
-                fig1 = go.Figure()
+                # Transposer le DataFrame
+                client_info_df = client_info_df.set_index("Attribute")
 
-                for target_value, (status, color) in zip([0, 1], colors.items()):
-                    # Filtrer les clients selon le statut
-                    status_clients = all_clients_df[all_clients_df['TARGET'] == target_value]
-                    fig1.add_trace(go.Scatter(
-                        x=status_clients['AMT_INCOME_TOTAL'],
-                        y=status_clients['AMT_CREDIT'],
-                        mode='markers',
-                        marker=dict(color=color, opacity=0.7),
-                        name=status.capitalize()  # Label pour la légende
-                    ))
+                # Afficher le tableau dans la barre latérale
+                st.sidebar.table(client_info_df)
 
-                # Ajouter le client spécifique
+                # Récupérer les données pour la visualisation
+                client_income = client_info['AMT_INCOME_TOTAL']
+                client_credit = client_info['AMT_CREDIT']
+                client_age = client_info['age']
+                client_children = client_info['CNT_CHILDREN']
+
+                # Récupérer les données de tous les clients pour la comparaison
+                all_clients = st.session_state.client_info.get("all_clients", [])
+                all_clients_df = pd.DataFrame(all_clients)
+                labels = ['Accepted', 'Rejected', 'Client']    
+
+                # Enregistrer les données dans session_state
+                st.session_state.income_comparison_data = (labels, client_income, client_credit, client_age, client_children)
+
+            else:
+                st.sidebar.error(st.session_state.client_info["error"])
+            st.write("Please enter a Client ID and click 'Get Prediction'.")
+            
+    # Paramètres de couleur pour le fond noir et le texte blanc
+    plt.style.use('dark_background')
+
+    # Bouton pour afficher la comparaison des caractéristiques du client
+    if st.sidebar.button("Compare Client") and "income_comparison_data" in st.session_state:
+        # Récupérer les données de comparaison
+        labels, client_income, client_credit, client_age, client_children = st.session_state.income_comparison_data
+
+        # Extraire uniquement les couleurs 'accepted' et 'rejected'
+        colors = {'accepted': ACCESSIBLE_COLORS['accepted'], 'rejected': ACCESSIBLE_COLORS['rejected']}
+        text_color = ACCESSIBLE_COLORS['text']
+
+        # Graphiques
+        if not all_clients_df.empty:
+            # 1. Graphique de distribution entre Income et Credit
+            fig1 = go.Figure()
+
+            for target_value, (status, color) in zip([0, 1], colors.items()):
+                # Filtrer les clients selon le statut
+                status_clients = all_clients_df[all_clients_df['TARGET'] == target_value]
                 fig1.add_trace(go.Scatter(
-                    x=[client_income],
-                    y=[client_credit],
+                    x=status_clients['AMT_INCOME_TOTAL'],
+                    y=status_clients['AMT_CREDIT'],
                     mode='markers',
-                    marker=dict(color='black', size=10, line=dict(color='black', width=2)),
-                    name="Client",
-                    text=[f"Income: €{client_income}<br>Credit: €{client_credit}"],
-                    hoverinfo="text"
+                    marker=dict(color=color, opacity=0.7),
+                    name=status.capitalize()  # Label pour la légende
                 ))
 
-                fig1.update_layout(
-                    title="Income vs Credit Distribution",
-                    xaxis_title="Income (€)",
-                    yaxis_title="Credit (€)",
-                    legend=dict(x=0, y=1)
+            # Ajouter le client spécifique
+            fig1.add_trace(go.Scatter(
+                x=[client_income],
+                y=[client_credit],
+                mode='markers',
+                marker=dict(color='black', size=10, line=dict(color='black', width=2)),
+                name="Client",
+                text=[f"Income: €{client_income}<br>Credit: €{client_credit}"],
+                hoverinfo="text"
+            ))
+
+            fig1.update_layout(
+                title="Income vs Credit Distribution",
+                xaxis_title="Income (€)",
+                yaxis_title="Credit (€)",
+                legend=dict(x=0, y=1)
+            )
+
+            st.plotly_chart(fig1)
+
+            # 2. Graphique de comparaison des âges moyens
+            accepted_age_avg = -all_clients_df[all_clients_df['TARGET'] == 0]['DAYS_BIRTH'].mean() / 365
+            rejected_age_avg = -all_clients_df[all_clients_df['TARGET'] == 1]['DAYS_BIRTH'].mean() / 365
+            
+            fig2 = go.Figure()
+            fig2.add_trace(go.Bar(
+                x=['Accepted', 'Rejected', 'Client'],
+                y=[accepted_age_avg, rejected_age_avg, client_age],
+                marker_color=[ACCESSIBLE_COLORS['accepted'], ACCESSIBLE_COLORS['rejected'], 'black'],  # Utiliser les couleurs acceptées et refusées
+            ))
+
+            fig2.update_layout(
+                title="Average Age Comparison",
+                yaxis_title="Age (Years)"
+            )
+            
+            st.plotly_chart(fig2)
+
+            # 3. Graphiques camembert pour la répartition de Gender, Loan Type et Income Type pour clients acceptés et refusés
+            for attribute, title in zip(['CODE_GENDER', 'NAME_CONTRACT_TYPE', 'NAME_INCOME_TYPE'],
+                                        ['Gender', 'Loan Type', 'Income Type']):
+                # Création de la figure avec des sous-graphiques
+                fig = make_subplots(
+                rows=1, 
+                cols=2, 
+                subplot_titles=('Accepted', 'Rejected'),
+                specs=[[{'type': 'pie'}, {'type': 'pie'}]]  
                 )
 
-                st.plotly_chart(fig1)
-
-                # 2. Graphique de comparaison des âges moyens
-                accepted_age_avg = -all_clients_df[all_clients_df['TARGET'] == 0]['DAYS_BIRTH'].mean() / 365
-                rejected_age_avg = -all_clients_df[all_clients_df['TARGET'] == 1]['DAYS_BIRTH'].mean() / 365
+                # Répartition pour clients acceptés
+                accepted_values = all_clients_df[all_clients_df['TARGET'] == 0][attribute].value_counts()
+                accepted_labels = accepted_values.index.tolist()
+                accepted_values = accepted_values.values.tolist()
                 
-                fig2 = go.Figure()
-                fig2.add_trace(go.Bar(
-                    x=['Accepted', 'Rejected', 'Client'],
-                    y=[accepted_age_avg, rejected_age_avg, client_age],
-                    marker_color=[ACCESSIBLE_COLORS['accepted'], ACCESSIBLE_COLORS['rejected'], 'black'],  # Utiliser les couleurs acceptées et refusées
-                ))
+                # Ajout du camembert pour les clients acceptés
+                fig.add_trace(go.Pie(
+                    labels=accepted_labels,
+                    values=accepted_values,
+                    name='Accepted',
+                    marker=dict(colors=plotly.colors.qualitative.Vivid)
+                ), row=1, col=1)
 
-                fig2.update_layout(
-                    title="Average Age Comparison",
-                    yaxis_title="Age (Years)"
+                # Répartition pour clients refusés
+                rejected_values = all_clients_df[all_clients_df['TARGET'] == 1][attribute].value_counts()
+                rejected_labels = rejected_values.index.tolist()
+                rejected_values = rejected_values.values.tolist()
+
+                # Ajout du camembert pour les clients refusés
+                fig.add_trace(go.Pie(
+                    labels=rejected_labels,
+                    values=rejected_values,
+                    name='Rejected',
+                    marker=dict(colors=plotly.colors.qualitative.Vivid)
+                ), row=1, col=2)
+
+                fig.update_layout(
+                    title=f"{title} Distribution",
+                    showlegend=True
                 )
-                
-                st.plotly_chart(fig2)
 
-                # 3. Graphiques camembert pour la répartition de Gender, Loan Type et Income Type pour clients acceptés et refusés
-                for attribute, title in zip(['CODE_GENDER', 'NAME_CONTRACT_TYPE', 'NAME_INCOME_TYPE'],
-                                            ['Gender', 'Loan Type', 'Income Type']):
-                    # Création de la figure avec des sous-graphiques
-                    fig = make_subplots(
-                    rows=1, 
-                    cols=2, 
-                    subplot_titles=('Accepted', 'Rejected'),
-                    specs=[[{'type': 'pie'}, {'type': 'pie'}]]  
-                    )
+                # Affichage du graphique dans Streamlit
+                st.plotly_chart(fig)
 
-                    # Répartition pour clients acceptés
-                    accepted_values = all_clients_df[all_clients_df['TARGET'] == 0][attribute].value_counts()
-                    accepted_labels = accepted_values.index.tolist()
-                    accepted_values = accepted_values.values.tolist()
-                    
-                    # Ajout du camembert pour les clients acceptés
-                    fig.add_trace(go.Pie(
-                        labels=accepted_labels,
-                        values=accepted_values,
-                        name='Accepted',
-                        marker=dict(colors=plotly.colors.qualitative.Vivid)
-                    ), row=1, col=1)
-
-                    # Répartition pour clients refusés
-                    rejected_values = all_clients_df[all_clients_df['TARGET'] == 1][attribute].value_counts()
-                    rejected_labels = rejected_values.index.tolist()
-                    rejected_values = rejected_values.values.tolist()
-
-                    # Ajout du camembert pour les clients refusés
-                    fig.add_trace(go.Pie(
-                        labels=rejected_labels,
-                        values=rejected_values,
-                        name='Rejected',
-                        marker=dict(colors=plotly.colors.qualitative.Vivid)
-                    ), row=1, col=2)
-
-                    fig.update_layout(
-                        title=f"{title} Distribution",
-                        showlegend=True
-                    )
-
-                    # Affichage du graphique dans Streamlit
-                    st.plotly_chart(fig)
-
-        # Show Feature Importance button only if the prediction is valid
-        if st.session_state.prediction_data and "error" not in st.session_state.prediction_data:
-            st.session_state.show_feature_importance = True
+    # Show Feature Importance button only if the prediction is valid
+    if st.session_state.prediction_data and "error" not in st.session_state.prediction_data:
+        st.session_state.show_feature_importance = True
 
         # Bouton pour afficher l'importance des features (Show Feature Importance)
         if st.sidebar.button("Feature Importances"):
@@ -494,8 +494,7 @@ if st.button("Get Prediction"):
                     st.pyplot(fig)
                 else:
                     st.error(f"Error fetching mean SHAP importances: {response.status_code} - {response.text}")
-
             else:
                 st.error("No SHAP importances available. Please check the prediction response.")
-                
-        st.session_state.client_info = None 
+            
+    st.session_state.client_info = None 
